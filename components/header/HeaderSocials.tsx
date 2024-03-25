cba4ea4dbb88b7fe4acb7929ac027766ae6e7759
Anuradha Singh <115535492+Anuradha-04@users.noreply.github.com>
2024-03-26 00:58:41 +0530

    Update HeaderSocials.tsx

--- components/header/HeaderSocials.tsx
+++ components/header/HeaderSocials.tsx
@@ -6,8 +6,8 @@ const HeaderSocials = () => {
     return (
         <div className='flex flex-col items-center gap-2 absolute left-0 bottom-12'>
             <a href='www.linkedin.com' target="_blank" className='text-primary'><BsLinkedin /></a>
-            <a href='www.github.com' target="_blank" className='text-primary'><BsGithub /></a>
-            <a href='mailto:youremail@example.com' className='text-primary'><TfiEmail /></a>
+            <a href='https://github.com/Anuradha-04' target="_blank" className='text-primary'><BsGithub /></a>
+            <a href='mailto:anuradhasingh93661@gmail.com' className='text-primary'><TfiEmail /></a>
         </div>
     );
 }
