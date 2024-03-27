58d4cbf9c956b358143ca9e41c9cf98cbb0676e3
Anuradha Singh <115535492+Anuradha-04@users.noreply.github.com>
2024-03-27 19:59:12 +0530

    Update HeaderSocials.tsx

--- components/header/HeaderSocials.tsx
+++ components/header/HeaderSocials.tsx
@@ -4,10 +4,10 @@ import { TfiEmail } from 'react-icons/tfi';
 
 const HeaderSocials = () => {
     return (
-        <div className='flex flex-col items-center gap-2 absolute left-0 bottom-12'>
-            <a href='https://www.linkedin.com/in/anuradha-singh-230b8b253/' target="_blank" className='text-primary'><BsLinkedin /></a>
-            <a href='https://github.com/Anuradha-04' target="_blank" className='text-primary'><BsGithub /></a>
-            <a href='mailto:anuradhasingh93661@gmail.com' className='text-primary'><TfiEmail /></a>
+        <div className='flex flex-col items-center gap-2 absolute left-0 bottom-12 '>
+            <a href='https://www.linkedin.com/in/anuradha-singh-230b8b253/' target="_blank" className='text-[#FFF]'><BsLinkedin /></a>
+            <a href='https://github.com/Anuradha-04' target="_blank" className='text-[#FFF]'><BsGithub /></a>
+            <a href='mailto:anuradhasingh93661@gmail.com' className='text-[#FFF]'><TfiEmail /></a>
         </div>
     );
 }
